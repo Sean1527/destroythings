d268bee19b18a4d25a2895b0c36793197f4a56ff
Sean1527 <sunliang1527@gmail.com>
2023-05-24 12:36:25 +0800

    增加元素的枚举和个性化配置

--- assets/scripts/Objects/TargetCtrl.ts
+++ assets/scripts/Objects/TargetCtrl.ts
@@ -2,10 +2,21 @@ import { _decorator, Component, Node } from 'cc';
 import { PlayerCtrl } from './PlayerCtrl';
 const { ccclass, property } = _decorator;
 
+enum TargetType {
+    Building = 0,
+    People = 1,
+    Animal = 2,
+    Buff = 3,
+  };
+
 @ccclass('TargetCtrl')
 export class TargetCtrl extends Component {
 
+    @property()
     private m_cur_value:number = 3.0;
+
+    //需要将类型目标类型暴露出去做成可配置的，然后根据这个类型实现不同的反馈效果
+    
 
     start() {
 
